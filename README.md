be7bf966763d4e65ff549de89ed2706b0472cecf
Mark Collins <tera_1225@hotmail.com>
2021-05-04 23:11:20 +0200

    Made into a self-contained python linter, using Requests

--- README.md
+++ README.md
@@ -1,21 +1,17 @@
 SublimeLinter-contrib-csl
-=================
+=========================
 
 [![Build Status](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-csl.svg?branch=master)](https://travis-ci.org/SublimeLinter/SublimeLinter-contrib-csl)
 
-This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides an interface to the [csl-validator.sh](https://github.com/citation-style-language/utilities) script. It will be used with files that have the Citation Style Language (CSL) syntax.
+This linter plugin for [SublimeLinter](https://github.com/SublimeLinter/SublimeLinter) provides web-based verification of Citation Style Language (CSL) files. It is a self-contained python linter based on [Requests](https://docs.python-requests.org).
 
 ## Installation
 SublimeLinter must be installed in order to use this plugin.
 
 Please use [Package Control](https://packagecontrol.io) to install the linter plugin.
 
-Before installing this plugin, you must ensure that the `csl-validator.sh` script from https://github.com/citation-style-language/utilities is available on your system. This further requires the `curl` and `jq` utilities.
-
-In order for `csl-validator.sh` to be executed by SublimeLinter, you must ensure that its path is available to SublimeLinter. The docs cover [troubleshooting PATH configuration](http://sublimelinter.readthedocs.io/en/latest/troubleshooting.html#finding-a-linter-executable).
-
 ## Settings
 - SublimeLinter settings: http://sublimelinter.readthedocs.org/en/latest/settings.html
 - Linter settings: http://sublimelinter.readthedocs.org/en/latest/linter_settings.html
 
-There are no specific settings for `csl-validator.sh` nor this plugin.
+There are no specific settings for this plugin.
--- linter.py
+++ linter.py
@@ -1,10 +1,61 @@
-import os
-from SublimeLinter.lint import Linter
+import json
+import requests
+import socket
 
-class SublimeLinterCsl (Linter):
-    linter_script = os.path.join(os.path.dirname(__file__), "csl-validator.sh")
-    cmd = (linter_script,) # comma is needed so that quoting is applied
+from SublimeLinter.lint import LintMatch, PythonLinter
+
+def offline(host="8.8.8.8", port=53, timeout=3):
+    # Adapted from https://stackoverflow.com/a/33117579
+    try:
+        socket.setdefaulttimeout(timeout)
+        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        return False
+    except socket.error as ex:
+        return True
+
+class SublimeLinterContribCsl (PythonLinter):
+    cmd = None
     regex = r'^M="(?P<message>.+)" L=(?P<line>\d+) C=(?P<col>\d+)$'
     defaults = {
-      'selector': 'source.csl, text.xml'
+        'selector': 'source.csl, text.xml'
     }
+
+    def run(self, cmd, code):
+        if offline():
+            print("No internet. Not linting CSL.")
+            return
+        url = "https://validator.w3.org/nu/"
+        files = { 'file': ('style.csl', code) }
+        data = {
+            'schema': 'https://raw.githubusercontent.com/citation-style-language/schema/v1.0.1/csl.rnc https://raw.githubusercontent.com/citation-style-language/schema/602ad40976b7b455a3ce0b79f5534e8e75f088e9/csl.sch',
+            'parser': 'xml',
+            'laxtype': 'yes',
+            'level': 'error',
+            'out': 'json',
+            'showsource': 'no'
+        }
+        response = requests.post(url, data=data, files=files)
+        return(response.text)
+
+    def find_errors(self, output):
+        try:
+            content = json.loads(output)
+        except ValueError:
+            logger.error(
+                "JSON Decode error: We expected JSON from 'csl', "
+                "but instead got this:\n{}\n\n"
+                .format(output)
+            )
+            self.notify_failure()
+            return
+
+        for error in content['messages']:
+            yield LintMatch(
+                match=error,
+                line=error['firstLine'] - 1 if 'firstLine' in error else error['lastLine'] - 1,
+                col=error['firstColumn'] if 'firstColumn' in error else 0,
+                message=error['message'],
+                error_type='error',
+                code='',
+            )
+
