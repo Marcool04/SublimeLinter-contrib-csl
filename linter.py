45fe1d6a1d2f09b1680b3be17ee4e52199a285b7
Mark Collins <tera_1225@hotmail.com>
2021-05-04 23:23:35 +0200

    Implement logger properly, and line_col_base

--- linter.py
+++ linter.py
@@ -1,8 +1,10 @@
 import json
+import logging
 import requests
 import socket
-
 from SublimeLinter.lint import LintMatch, PythonLinter
+
+logger = logging.getLogger(__name__)
 
 def offline(host="8.8.8.8", port=53, timeout=3):
     # Adapted from https://stackoverflow.com/a/33117579
@@ -22,7 +24,7 @@ class SublimeLinterContribCsl (PythonLinter):
 
     def run(self, cmd, code):
         if offline():
-            print("No internet. Not linting CSL.")
+            logger.info("Offline, not linting CSL.")
             return
         url = "https://validator.w3.org/nu/"
         files = { 'file': ('style.csl', code) }
@@ -42,7 +44,7 @@ class SublimeLinterContribCsl (PythonLinter):
             content = json.loads(output)
         except ValueError:
             logger.error(
-                "JSON Decode error: We expected JSON from 'csl', "
+                "JSON Decode error: We expected JSON from CSL, "
                 "but instead got this:\n{}\n\n"
                 .format(output)
             )
@@ -52,8 +54,8 @@ class SublimeLinterContribCsl (PythonLinter):
         for error in content['messages']:
             yield LintMatch(
                 match=error,
-                line=error['firstLine'] - 1 if 'firstLine' in error else error['lastLine'] - 1,
-                col=error['firstColumn'] if 'firstColumn' in error else 0,
+                line=error['firstLine'] - self.line_col_base[0] if 'firstLine' in error else error['lastLine'] - self.line_col_base[0],
+                col=error['firstColumn'] - self.line_col_base[1] if 'firstColumn' in error else 0,
                 message=error['message'],
                 error_type='error',
                 code='',
