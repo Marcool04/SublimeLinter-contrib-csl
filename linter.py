a7e7db2dbb31eee2d7199882b1ad5242f44d955b
Mark Collins <tera_1225@hotmail.com>
2021-05-04 12:00:38 +0200

    Include csl-validator.sh script in packag and git update-index --chmod=+x csl-validator.sh

--- linter.py
+++ linter.py
@@ -1,7 +1,9 @@
+import os
 from SublimeLinter.lint import Linter
 
 class SublimeLinterCsl (Linter):
-    cmd = 'csl-validator.sh'
+    linter_script = os.path.join(os.path.dirname(__file__), "csl-validator.sh")
+    cmd = (linter_script,) # comma is needed so that quoting is applied
     regex = r'^M="(?P<message>.+)" L=(?P<line>\d+) C=(?P<col>\d+)$'
     defaults = {
       'selector': 'source.csl, text.xml'
